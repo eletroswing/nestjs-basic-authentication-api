5c671dd962f6d79eca23aeae0e67118c60834b50
eletroswing <ytalosilvabatalha@gmail.com>
2022-02-25 15:00:36 -0300

    NestJS init project

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <h1>NestJS basic api for authentication</h1>
+</p>
---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  IsApiOnline(){
+    return {message: 'online'};
+  }
+}
